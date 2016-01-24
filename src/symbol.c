8d176e2d38635d1c9b2bde915205f39390623333
David Hunt <huntdog1541@gmail.com>
2016-01-23 22:11:05 -0600

    updated symbol
    updated symbol table

--- src/symbol.c
+++ src/symbol.c
@@ -12,3 +12,44 @@
  #include "lexer.h"
  #include "parser.h"
  #include "symbol.h"
+
+ #define STRMAX 999
+ #define SYMMAX 999
+
+ struct entry {
+   char * lexptr;
+   int token;
+ };
+
+ struct entry keywords[] = {
+   "begin", BEGIN,
+   "end", END,
+   "div", DIV,
+   "mod", MOD,
+   0, 0
+ };
+
+ init()
+ {
+   struct entry *p;
+   for(p = keywords; p->token; p++)
+      insert(p->lexptr, p->token);
+ }
+
+ int lookup(char s[])
+ {
+   int p;
+   for(p = lastentry; p > 0; p--)
+    if(! strcmp(symtable[p].lexptr, s) )
+      return p;
+
+    return NOT_FOUND;
+ }
+
+ int insert(char s[], int tok)
+ {
+   int len;
+   len = strlen(s);
+   if(lastentry + 1 >= SYMMAX)
+    
+ }
