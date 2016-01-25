1e184087b5e6d644b182746c80b59eb68b040b12
David Hunt <huntdog1541@gmail.com>
2016-01-24 20:53:20 -0600

    updated symbol.c

--- src/symbol.c
+++ src/symbol.c
@@ -16,6 +16,14 @@
  #define STRMAX 999
  #define SYMMAX 999
 
+struct entry symtable[SYMMAX];
+
+char lexemes[STRMAX];
+
+int lastchar = -1;
+int lastentry = 0;
+
+
  struct entry {
    char * lexptr;
    int token;
@@ -33,8 +41,7 @@
    "float", FLOAT,
    "char", CHAR,
    "string", STRING,
-
-   "mod", MOD,
+   "%", MODUS,
    0, 0
  };
 
@@ -60,5 +67,30 @@
    int len;
    len = strlen(s);
    if(lastentry + 1 >= SYMMAX)
-
+      error("symbol table full");
+   if(lastchar + len + 1 >= STRMAX)
+      error("lexeme array full");
+   lastentry = lastentry + 1;
+   symtable[lastentry].token = tok;
+   symtable[lastentry].lexptr = &lexemes[lastchar + 1];
+   lastchar = lastchar + len + 1;
+   strcpy(symtable[lastentry].lexptr, s);
+   return lastentry;
  }
+
+char * getLexeme(int pos)
+{
+  return symtable[pos].lexptr;
+}
+
+int getTokenType(int pos)
+{
+  return symtable[pos].token;
+}
+
+void printSymbol()
+{
+    struct entry *p;
+    for(p = keywords; p->token; p++)
+      printf("KEYWORD: %s and value %d\n", p->lexptr, p->token);
+}
