3e7b588f0168c061e862e42fcd3110cf8e9f65ef
David Hunt <huntdog1541@gmail.com>
2016-02-19 18:43:45 -0600

    added node.h node.c

---
+++ src/node.c
@@ -0,0 +1,64 @@
+/*
+*  Node.c - by David Hunt
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "node.h"
+
+struct node * createNode()
+{
+  struct node * temp = (struct node *)calloc(sizeof(struct node));
+  temp->tokenNumber = NULL;
+  temp->loperand = NULL;
+  temp->roperand = NULL;
+  temp->nextNode = NULL;
+  temp->next     = NULL;
+  return temp;
+}
+
+struct nodeHandler * createRoot(struct nodeHandler * hand)
+{
+  hand->root = createNode();
+  return hand;
+}
+
+struct node * bindNode(struct node * currentNode, struct node * tempNode)
+{
+  currentNode->nextNode = tempNode;
+  return tempNode;
+}
+
+struct node * addLOP(struct node * currentNode)
+{
+  currentNode->loperand = createNode();
+  return currentNode->loperand;
+}
+
+struct node * addROP(struct node * currentNode)
+{
+  currentNode->roperand = createNode();
+  return currentNode->roperand;
+}
+
+struct nodeHandler * destroyTree(struct nodeHandler * hand)
+{
+  hand->tempNode = NULL;
+  hand->currentNode = NULL;
+  hand->root = destroyNode(hand->root);
+  return hand;
+}
+
+struct node * destroyNode(struct node * temp)
+{
+  if(temp->nextNode != NULL)
+    destroyNode(temp->nextNode);
+  if(temp->loperand != NULL)
+    destroyNode(temp->loperand);
+  if(temp->roperand = NULL)
+    destroyNode(temp->roperand);
+  if(temp->next != NULL)
+    destroyNode(temp->next);
+  temp = free(temp);
+  return temp;
+}
---
+++ src/node.h
@@ -0,0 +1,43 @@
+/*
+*  Node.h - David Hunt
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#ifndef NODE_H_
+#define NODE_H_
+
+#define BUFFERSIZE 256
+
+struct node {
+  char tokenValue[BUFFERSIZE];
+  int tokenNumber;
+  struct node * loperand;
+  struct node * roperand;
+  struct node * nextNode;
+  struct node * next;
+}
+
+struct nodeHandler {
+  struct node * root;
+  struct node * currentNode;
+  struct node * tempNode;
+}
+
+struct node * createNode();
+
+struct node * createRoot(struct nodeHandler * hand);
+
+struct node * bindNode(struct node * currentNode, struct node * tempNode);
+
+struct node * addLOP(struct node * currentNode);
+
+struct node * addROP(struct node * currentNode);
+
+struct nodeHandler * destroyTree(struct nodeHandler * hand);
+
+struct node * destroyNode(struct node * temp);
+
+#endif
